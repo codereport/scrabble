b5153ac56a00d15d467bba8b7d000db452729277
Conor Hoekstra <codereport@outlook.com>
2022-11-16 22:03:06 -0500

    Add tile points to tiles

--- scrabble/python/main.py
+++ scrabble/python/main.py
@@ -298,6 +298,9 @@ class MyGame(arcade.Window):
         if not self.just_bingoed and pos in self.letters_bingoed:
             arcade.draw_rectangle_outline(x, y, WIDTH-4, HEIGHT-4, arcade.color.DARK_PASTEL_GREEN, 5)
         arcade.draw_text(letter, x-HORIZ_TEXT_OFFSET, y-VERT_TEXT_OFFSET, arcade.color.WHITE, FONT_SIZE, bold=True, font_name='mono')
+        if letter != ' ':
+            value = TILE_SCORE[letter.upper()]
+            arcade.draw_text(value, x-HORIZ_TEXT_OFFSET + 27, y-VERT_TEXT_OFFSET - 6, arcade.color.WHITE, 10, bold=True, font_name='mono')
 
     def on_draw(self):
         """Render the screen"""
