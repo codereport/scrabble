2b3dc464258f669ce0fe3ad0a6708799e9d06707
Conor Hoekstra <codereport@outlook.com>
2022-11-20 18:07:08 -0500

    Set correct number of blanks

--- scrabble/python/main.py
+++ scrabble/python/main.py
@@ -68,7 +68,7 @@ TILE_BAG = \
     ['A'] * 9 + ['B'] * 2 + ['C'] * 2 + ['D'] * 4 + ['E'] * 12 + ['F'] * 2 + ['G'] * 3 + \
     ['H'] * 2 + ['I'] * 9 + ['J'] * 1 + ['K'] * 1 + ['L'] * 4  + ['M'] * 2 + ['N'] * 6 + \
     ['O'] * 8 + ['P'] * 2 + ['Q'] * 1 + ['R'] * 6 + ['S'] * 4  + ['T'] * 6 + ['U'] * 4 + \
-    ['V'] * 2 + ['W'] * 2 + ['X'] * 1 + ['Y'] * 2 + ['Z'] * 1  + [' '] * 10
+    ['V'] * 2 + ['W'] * 2 + ['X'] * 1 + ['Y'] * 2 + ['Z'] * 1  + [' '] * 2
 
 LR_ARROW_KEYS = [arcade.key.LEFT, arcade.key.RIGHT]
 UD_ARROW_KEYS = [arcade.key.UP, arcade.key.DOWN]
@@ -425,7 +425,7 @@ class MyGame(arcade.Window):
         y = 50
         arcade.draw_text(self.definition, x-HORIZ_TEXT_OFFSET, y-VERT_TEXT_OFFSET + 20, arcade.color.WHITE, 9, font_name='mono')
 
-        left     = max(0, 98 - self.tile_bag_index + len(self.computer.tiles))
+        left     = max(0, 100 - self.tile_bag_index + len(self.computer.tiles))
         tile_bag = ' '.join(f"{a}:{b}" for a,b in sorted(Counter(TILE_BAG[self.tile_bag_index:] + self.computer.tiles).items()))
         arcade.draw_text(f"{left} {tile_bag}", x-HORIZ_TEXT_OFFSET, y-VERT_TEXT_OFFSET, arcade.color.WHITE, 9, font_name='mono')
 
